7233afd0247e3cdd9475191de8c8dca40970a153
Kayla Dale <kmeyers119@gmail.com>
2024-02-22 21:25:13 -0600

    initial commit

---
+++ src/test/java/edu/mcc/codeschool/inventorySystem/InventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mcc.codeschool.inventorySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
